4c2dc14140db3c23fbea9a8f3649948f7976ee9d
tolentinoel <rckrleyn@gmail.com>
2020-12-03 14:38:14 -0800

    sorting user options done, wip on functions to render the game

--- src/index.js
+++ src/index.js
@@ -1,67 +1,137 @@
 const userUrl = 'https://localhost:3000/users'
-
+const login = document.querySelector('form')
 // Welcome page should only have username form and disabled board
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const login = document.querySelector('form')
     login.addEventListener("submit", (e) => {
         // console.log(e)
         renderSideNav(e)
     })
 })
-
+// <<<<<<-------------BUILDING ALL ELEMENTS FOR GAME OPTIONS------------------>>>>>>>
 function renderSideNav(event){
     event.preventDefault()
 
-    const userName = event.target.querySelector('input').value
-    const nameDiv = document.getElementById('name-display')
-    const nameText = document.createElement('h2')
-    nameText.innerText = `${userName}`
-    nameDiv.appendChild(nameText)
-
     // event.target.reset()
-
     event.target.querySelector('input').setAttribute('disabled', " ")
-    event.target.querySelector("input[type='submit']").setAttribute('style',"display:none")
-
+    event.target.querySelector('#submitName').setAttribute('style',"display:none")
 
 
     const choices = document.querySelector('#options')
     choices.innerHTML = `
-    <div id="checkDiv">
-        <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="customCheck1">
-            <label class="custom-control-label" for="customCheck1">Easy</label>
+
+    <div id="difDiv">
+        <div class="custom-control custom-radio">
+            <input type="radio" id="customRadio1a" name="customRadio1" class="custom-control-input" required value="easy">
+            <label class="custom-control-label" for="customRadio1a">Easy</label>
         </div>
-        <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="customCheck1">
-            <label class="custom-control-label" for="customCheck2">Medium</label>
+        <div class="custom-control custom-radio">
+            <input type="radio" id="customRadio1b" name="customRadio1" class="custom-control-input" required value='medium'>
+            <label class="custom-control-label" for="customRadio1b">Medium</label>
         </div>
-        <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="customCheck1">
-            <label class="custom-control-label" for="customCheck3">Hard</label>
+        <div class="custom-control custom-radio">
+            <input type="radio" id="customRadio1c" name="customRadio1" class="custom-control-input" required value="hard">
+            <label class="custom-control-label" for="customRadio1c">Hard</label>
         </div>
     </div>
-    
+
     <div id="radioDiv">
         <p>THEMES:</p>
         <div class="custom-control custom-radio">
-            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-            <label class="custom-control-label" for="customRadio1">Default</label>
+            <input type="radio" id="customRadio2a" name="customRadio2" class="custom-control-input" required value="default">
+            <label class="custom-control-label" for="customRadio2a">Default</label>
         </div>
         <div class="custom-control custom-radio">
-            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-            <label class="custom-control-label" for="customRadio2">Rick and Morty</label>
+            <input type="radio" id="customRadio2b" name="customRadio2" class="custom-control-input" required value='rickMorty'>
+            <label class="custom-control-label" for="customRadio2b">Rick and Morty</label>
         </div>
         <div class="custom-control custom-radio">
-            <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input">
-            <label class="custom-control-label" for="customRadio3">Vectors</label>
+            <input type="radio" id="customRadio2c" name="customRadio2" class="custom-control-input"  required value="vectors">
+            <label class="custom-control-label" for="customRadio2c">Vectors</label>
         </div>
     </div>
-    <input type="submit" value="Click to Play!" id="startPlay">`
+    <button type="submit" class="btn btn-success" id="startPlay">Click to Play!</button>`
 
+    const radios = document.querySelectorAll("input[type='radio']")
+    radios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            // debugger
+            enabledSettings =
+            Array.from(radios) // Convert radio to an array to use filter and map. Expecting [<difficulty>, <theme>]
+            .filter(i => i.checked) // Use Array.filter to remove unchecked radio.
+            .map(i => i.value) // Use Array.map to extract only the values from the array of objects.
 
+            const playBtn = document.querySelector('#startPlay')
+
+            playBtn.addEventListener('click', (ev) => {
+                renderGame(ev, enabledSettings)
+
+            })
+        })
+    })
+    let userName = document.querySelector('#user').value
+    const nameDiv = document.getElementById('name-display')
+    const nameText = document.createElement('h2')
+    nameText.innerText = `${userName}`
+    nameDiv.appendChild(nameText)
+}
+
+// <<<<--------WHEN EVENT IS CONSOLE.LOGGED, 2 OF THE SAME EVENT LOGS-- A PROBLEM?---->>>>>>>
+function renderGame(ev, settings){
+    ev.preventDefault()
+
+    switch (settings[0]) {
+        case
+            "easy":
+            easyGame()
+            break;
+        case
+            "medium":
+            mediumGame()
+            break;
+        case
+            "hard":
+            hardGame()
+
+    }
+    switch (settings[1]) {
+        case
+            "default":
+            themeDefault()
+            break;
+        case
+            "rickMorty":
+            themeRickMorty()
+            break;
+        case
+            "vectors":
+            themeVector()
+            break;
+    }
+    const btn = document.querySelector("#startPlay")
+    btn.innerText = 'Quit Game'
+    btn.style.backgroundColor = "#c50f21"
+}
+
+function easyGame(){
 
 }
 
+function mediumGame(){
+
+}
+function hardGame(){
+
+}
+
+function themeDefault(){
+
+}
+
+function themeRickMorty(){
+
+}
+function themeVector(){
+
+}
