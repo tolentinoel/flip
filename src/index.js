94cf78eab64e7138c8857e6f52f7a64b1e8f12a5
tolentinoel <rckrleyn@gmail.com>
2020-12-06 03:49:06 -0800

    Easy mode icons up and sorting good,WIP on check logic

--- src/index.js
+++ src/index.js
@@ -1,24 +1,54 @@
 const USERS_URL ="http://localhost:3000/users"
 const BOARDS_URL = "http://localhost:3000/boards"
 const deck = document.querySelector('#card-deck')
+const ICON_API = []
+const RM_API = []
+let easyArray = ["fa fa-camera fa-3x","fa fa-candy-cane fa-3x", "fa fa-carrot fa-3x", "fa fa-cookie-bite fa-3x", "fa fa-gifts fa-3x", "fa fa-glass-cheers fa-3x", "fa fa-holly-berry fa-3x", "fa fa-sleigh fa-3x","fa fa-camera fa-3x","fa fa-candy-cane fa-3x", "fa fa-carrot fa-3x", "fa fa-cookie-bite fa-3x", "fa fa-gifts fa-3x", "fa fa-glass-cheers fa-3x", "fa fa-holly-berry fa-3x", "fa fa-sleigh fa-3x"];
 
-let easyArray = ['fa-anchor', 'fa-anchor', 'fa-bicycle', 'fa-bolt', 'fa-cube', 'fa-diamond', 'fa-diamond', 'fa-plane', 'fa-leaf', 'fa-bomb', 'fa-leaf', 'fa-bomb', 'fa-bolt', 'fa-bicycle', 'fa-plane', 'fa-cube'];
+
 
 // Welcome page should only have username form and disabled board
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    fetchIcons()
+    fetchRickMorty()
     const login = document.querySelector('form')
-    login.addEventListener("submit", renderSideNav)
+    login.addEventListener("submit", (e) => {
+        renderSideNav(e)
+
+    })
 
 
 })
+// <<<<<<-------------FETCHING IMG URLs FROM APIs------------------>>>>>>>
+function fetchIcons(){
+
+    fetch("https://api.iconfinder.com/v2/icons/search?query=fantasy")
+    .then(response => response.json())
+    .then(data => {
+        data.icons.forEach(obj => {
+            ICON_API.push(obj.raster_sizes[6].formats[0].preview_url)
+        })
+    })
+}
+
+function fetchRickMorty(){
+
+    fetch("https://rickandmortyapi.com/api/character/")
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(character => {
+            RM_API.push(character.image)
+
+            })
+        })
+
+}
 
 
 // <<<<<<-------------BUILDING ALL ELEMENTS FOR GAME OPTIONS------------------>>>>>>>
 function renderSideNav(event){
     event.preventDefault()
-
     const name = document.querySelector('input','type=text')
     const newData = {
         'username': name.value,
@@ -78,19 +108,17 @@ function renderSideNav(event){
     const radios = document.querySelectorAll("input[type='radio']")
     radios.forEach(radio => {
         radio.addEventListener('change', () => {
-            // debugger
+
             enabledSettings =
             Array.from(radios) // Convert radio to an array to use filter and map. Expecting [<difficulty>, <theme>]
             .filter(i => i.checked) // Use Array.filter to remove unchecked radio.
             .map(i => i.value) // Use Array.map to extract only the values from the array of objects.
 
-            const playBtn = document.querySelector('#startPlay')
-            // debugger
-            // console.log('outside of if')
             if (enabledSettings.length === 2) {
+                const playBtn = document.querySelector('#startPlay')
                 playBtn.addEventListener('click', (ev) => {
-                    createBoard(enabledSettings)
                     renderGame(ev, enabledSettings)
+                    createBoard(enabledSettings)
                 })
             }
         })
@@ -102,10 +130,11 @@ function renderSideNav(event){
     nameDiv.appendChild(nameText)
 }
 
+
+
 function createBoard(settings){
     const game_difficulty = `${settings[0]}`
     const game_theme = `${settings[1]}`
-
 
 // ___________________CREATING A BOARD IN THE DATABASE___________________
     fetch(BOARDS_URL, {
@@ -132,7 +161,6 @@ function createBoard(settings){
 
 }
 
-// <<<<--------WHEN EVENT IS CONSOLE.LOGGED, 2 OF THE SAME EVENT LOGS-- A PROBLEM?---->>>>>>>
 function renderGame(ev, settings){
     ev.preventDefault()
     switch (settings[0]) {
@@ -169,58 +197,38 @@ function renderGame(ev, settings){
     // console.log('rendered game')
 }
 
+
+// <<<<<---------------- GAME DIFFICULTY FUNCTIONS ------->>>>>>>>>>
+
+
 function easyGame(){
     // FIRST, RENDER THE BOARD EASY MODE
-    const num = deck.childElementCount
+    const num = 16
     // Empty the welcome deck aka disabled board
     deck.innerHTML = " "
-
     // loop over and make the same num of boxes/cards that was gone
     createCards(num)
 
 }
 
 function mediumGame(){
-    const count = deck.childElementCount + 8
-
-    document.querySelectorAll('.flip-card-front').forEach(card => {
-        card.style.width = "105px"
-        })
-    document.querySelectorAll('.flip-card').forEach(card => {
-        card.style.width = "105px"
-
-        })
-    document.querySelectorAll('.flip-card-container').forEach(card => {
-        card.style.width = '105px'
-
-        })
-    deck.style.width = "720px"
+    const count = 24
+    deck.style.width = "700px"
     deck.innerHTML = " "
     createCards(count)
 }
 
 function hardGame(){
-    const count = deck.childElementCount + 14
-
-    document.querySelectorAll('.flip-card-front').forEach(card => {
-        card.style.width = "75px"
-        })
-    document.querySelectorAll('.flip-card').forEach(card => {
-        card.style.width = "85px"
-
-        })
-    document.querySelectorAll('.flip-card-container').forEach(card => {
-        card.style.width = '85px'
-
-        })
-    deck.style.width = "720px"
-    deck.style.height = "600px"
+    const count = 36
+    deck.style.width = "800px"
+    deck.style.height = "485px"
     deck.innerHTML = " "
     createCards(count)
 }
 
 function createCards(number){
     for(i = 0;i < number; i++){
+
         const container = document.createElement('div')
         const cardDiv = document.createElement('div')
         const frontCard = document.createElement('div')
@@ -239,15 +247,82 @@ function createCards(number){
         container.classList.toggle('flip');
         })
 
-    // THIS SHOULD BE FUNC TO FLIP CARD
+// <<<----------CHECK IF USER CHOSE MEDIUM OR HARD TO POPULATE BOARD WITH MORE CARDS------->>>
+        if (number == 24 ){
+            document.querySelectorAll('.flip-card-front').forEach(card => {
+                card.style.width = "100px"
+                card.style.height = "100px"
+                })
+            document.querySelectorAll('.flip-card').forEach(card => {
+                card.style.width = "100px"
+                card.style.height = "100px"
+                })
+            document.querySelectorAll('.flip-card-container').forEach(card => {
+                card.style.width = "100px"
+                card.style.height = "100px"
+                })
 
+        } else if (number == 36){
+            document.querySelectorAll('.flip-card-front').forEach(card => {
+                card.style.width = "80px"
+                card.style.height = "100px"
+                })
+            document.querySelectorAll('.flip-card').forEach(card => {
+                card.style.width = "80px"
+                card.style.height = "100px"
+                })
+            document.querySelectorAll('.flip-card-container').forEach(card => {
+                card.style.width = "80px"
+                card.style.height = "100px"
+                })
+
+        }
     }
 }
 
 
 
 function themeDefault(){
+    // <<<<----------SELECTING ALL CARD-BACKS TO RENDER THE ICONS---------->>>>
+    const cardDiv = deck.querySelectorAll('.flip-card-back')
+    console.log(cardDiv)
 
+        easyArray.sort(() => Math.random() - 0.7)
+        for(let b = 0;b < cardDiv.length; b+=1){
+            const icon = document.createElement('i')
+            cardDiv[b].appendChild(icon)
+            icon.setAttribute('class', easyArray[b])
+
+        }
+
+    // -------PUTTING EMPTY CONTAINERS FOR EVENTS AND CLASSES FOR CHECK IF MATCH
+        const pair = []
+        const clicks = []
+        deck.addEventListener('click', (e) => {
+    // -------ADDING LISTENER FOR CLICKS AND GETTING CLASS NAME TO PAIR THEM
+            console.log("A CARD HAS BEEN CLICKED #1")
+            pair.push(e.target.parentElement.querySelector('i').className) //Expecting array [<iconclass>,<iconclass>]
+            // clicks.push(e.target.closest('.flip-card-container').class = 'flip-card-container flip')<<< pushes e.targets to clicks array, to be carried over to another function
+
+    // -------- DOING A CHECK FOR EVENTS IF USER CLICKED 2 CARDS
+            if (pair.length !== 2){
+                return pair
+            }else if (pair.length === 2){
+    //---------NEEDS TO DISABLE OTHER CARDS SO THAT USER CANT CLICK MORE THAN 2 BEFORE CHECKCARD FUNCTION
+                console.log("A CARD HAS BEEN CLICKED #2")
+
+                checkCard(pair)
+                return
+            }
+        })
+
+
+}
+
+function checkCard(arr){
+// ------- CHECK CARD CLASSES IF MATCH,
+//          Disable after animation?
+//--------- IF NOT, e.target.closest('.flip-card-container').class = 'flip-card-container flip' << in short flip it back
 }
 
 function themeRickMorty(){
@@ -256,3 +331,4 @@ function themeRickMorty(){
 function themeVector(){
 
 }
+
